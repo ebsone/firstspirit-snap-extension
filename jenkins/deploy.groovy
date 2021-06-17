2632d92b13d02b9c554c74a43a8f6b5a41d40cab
Andreas Mülhaupt <61550715+anmuelhauptsto@users.noreply.github.com>
2021-06-17 16:45:51 +0200

    Jenkinsfile added

---
+++ jenkins/deploy.groovy
@@ -0,0 +1,21 @@
+pipeline {
+    agent any
+    options {
+        disableConcurrentBuilds()
+        buildDiscarder(logRotator(numToKeepStr: '2'))
+    }
+
+    stages {
+        stage('Deploy to DEWEAP03') {
+            when { branch 'master' }
+            environment {
+                SSH_CONNECTION = 'jenkins@212.184.115.17'
+                DEPLOYMENT_PATH = '/usr/share/nginx/html/firstspirit-snap-extension/'
+            }
+            steps {
+                // PUBLISH TO LIVE SERVER
+                sh "scp -i '~jenkins/.ssh/id_rsa' *.js ${SSH_CONNECTION}:${DEPLOYMENT_PATH}"
+            }
+        }
+    }
+}
